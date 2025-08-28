5c557a5366c299168d04b6d5cd8e34e331c2fd59
asmaamaged2022 <asmaamaged96@gmail.com>
2025-08-28 22:24:10 +0300

    new

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let contener = document.getElementById("contaner");
 let screen = document.getElementById("display");
+let justCalculated = false;
 
 contener.addEventListener("click", function (e) {
   if (e.target.nodeName == "BUTTON") {
@@ -20,19 +21,37 @@ contener.addEventListener("click", function (e) {
 });
 function clear() {
   screen.textContent = "";
+  justCalculated = false;
 }
 function add(value) {
-  screen.textContent += value;
+  if (screen.textContent === "math error") {
+    screen.textContent = value;
+    justCalculated = false;
+    return;//to stop here
+  }
+  if (justCalculated) {
+    if (!isNaN(value) || value === ".") {
+      screen.textContent = value;
+    } else {
+      screen.textContent += value;
+    }
+    justCalculated = false;
+  } else {
+    screen.textContent += value;
+  }
 }
 function del() {
   let current = screen.textContent;
   screen.textContent = current.substring(0, current.length - 1);
+  justCalculated = false;
 }
 function calc() {
   try {
     let calculation = eval(screen.textContent);
     screen.textContent = calculation;
+    justCalculated = true;
   } catch (error) {
     screen.textContent = `math error`;
+    justCalculated = false;
   }
 }
