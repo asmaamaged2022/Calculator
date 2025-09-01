7847911b7e033f32f79b96e343748107e248e826
asmaamaged2022 <asmaamaged96@gmail.com>
2025-09-01 12:16:34 +0300

    new

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 let contener = document.getElementById("contaner");
 let screen = document.getElementById("display");
-let justCalculated = false;
+let justCalculated = true;
+const operators = ["+", "*", "/"];
+let op = ["+", "*", "/", "-"];
 
 contener.addEventListener("click", function (e) {
   if (e.target.nodeName == "BUTTON") {
@@ -21,29 +23,35 @@ contener.addEventListener("click", function (e) {
 });
 function clear() {
   screen.textContent = "";
-  justCalculated = false;
+  justCalculated = true;
 }
 function add(value) {
   if (screen.textContent === "math error") {
     screen.textContent = value;
     justCalculated = false;
-    return;//to stop here
+    return; //to stop here
   }
+
   if (justCalculated) {
-    if (!isNaN(value) || value === ".") {
+    if (!isNaN(value) || value === "." || value === "-") {
       screen.textContent = value;
-    } else {
-      screen.textContent += value;
+    } else if (screen.textContent.trim() === "" && operators.includes(value)) {
+      return;
     }
     justCalculated = false;
+  } else if (op.includes(value) && op.includes(screen.textContent.slice(-1))) {
+    return;
   } else {
     screen.textContent += value;
   }
 }
+
 function del() {
   let current = screen.textContent;
   screen.textContent = current.substring(0, current.length - 1);
-  justCalculated = false;
+  if (screen.textContent.length === 0) {
+    justCalculated = true;
+  }
 }
 function calc() {
   try {
@@ -52,6 +60,21 @@ function calc() {
     justCalculated = true;
   } catch (error) {
     screen.textContent = `math error`;
-    justCalculated = false;
+    justCalculated = true;
   }
 }
+document.addEventListener("keydown", function (e) {
+  if (!isNaN(e.key) || ["+", "-", "*", "/", "."].includes(e.key)) {
+    add(e.key);
+  }
+  if (e.key === "Escape") {
+    clear();
+  }
+  if (e.key === "Backspace") {
+    del();
+  }
+
+  if (e.key === "Enter") {
+    calc();
+  }
+});
